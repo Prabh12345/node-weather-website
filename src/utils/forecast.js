c6f1b1423c9cef789131a6293916d9debde3fa2c
Prabh12345 <42003221+Prabh12345@users.noreply.github.com>
2020-09-07 17:53:24 +0530

    Add feels like temperature

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,8 @@ const forecast=(latitude,longitude,callback)=>{
             callback('Unable to find location',undefined)
         }
         else{
-            callback(undefined,body.current.weather_descriptions[0]+'. It is currently '+body.current.temperature+' degrees out. There is a '+body.current.precip+'% chance of rain.')
+            console.log(body.current)
+            callback(undefined,body.current.weather_descriptions[0]+'. It is currently '+body.current.temperature+' degrees out. Feels like '+body.current.feelslike +' degrees out. There is a '+body.current.precip+'% chance of rain.')
         }
     })
 
